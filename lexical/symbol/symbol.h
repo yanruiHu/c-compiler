9833e225b6fa17f1691e7af4c614715a9d4d3c86
hkx-hkx <hkx@DESKTOP-EU4NBIF.localdomain>
2020-12-01 23:51:30 +0800

    modify symbol.h

--- lexical/symbol/symbol.h
+++ lexical/symbol/symbol.h
@@ -1,11 +1,14 @@
 #ifndef _SYMBOL_H_
 #define _SYMBOL_H_
+#include <string>
+#include <unordered_map>
+#include <vector>
 
 namespace SMB {
     enum SymbolType {
-        none = 0,
+        none = 0,       //undefine
         integer = 1,
-        pointer = 2,
+        pointer = 2,   
         boolean = 3,
         void_type = 4,
         homebrew = 5,   // for struct
@@ -13,6 +16,8 @@ namespace SMB {
         literal = 7,
         function = 8,
     };
+
+
 } // namespace SMB
 
 
