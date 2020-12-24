00a0e168b905975b299c3a113047264adc61e1de
srameo <870830732@qq.com>
2020-12-24 14:49:06 +0800

    添加数据

--- grammar/InterMediate/InterMediate.cpp
+++ grammar/InterMediate/InterMediate.cpp
@@ -1,2 +1,5 @@
 #include "./InterMediate.h"
 
+IM::InterMediate::InterMediate(AST::BaseNode *root) {
+    this->root_node = root;
+}
--- grammar/InterMediate/InterMediate.h
+++ grammar/InterMediate/InterMediate.h
@@ -4,16 +4,26 @@
 #include "./Triple.h"
 #include "../Nodes.h"
 #include "../symbol/symbol.h"
+#include <stack>
+#include <list>
 
 namespace IM {
 
     class InterMediate {
     private:
         AST::BaseNode *root_node;
-        SMB::SymbolTable *symbol_table;
+        // SMB::SymbolTable *symbol_table;
+
+        // bool expr
+        std::stack< std::list<int> > true_list;
+        std::stack< std::list<int> > false_list;
+
+        // makelist, merge, backpatch
+        
 
     public:
-        InterMediate(AST::BaseNode*, SMB::SymbolTable*);
+        InterMediate(AST::BaseNode*);
+        // InterMediate(AST::BaseNode*, SMB::SymbolTable*);
         void generate(AST::BaseNode*);
         
     };
